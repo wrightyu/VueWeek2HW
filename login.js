96c696f8b786ff5985f452c0586c0f21d3bc5af3
WrightYu <wright820328@gmail.com>
2023-01-06 00:53:25 +0800

    git init

---
+++ login.js
@@ -0,0 +1,30 @@
+const app = {
+  data() {
+    return {
+      baseUrl : "https://vue3-course-api.hexschool.io/v2",
+      userPath : "wrightyu",
+      userData: {
+        "username": "",
+        "password": ""
+      },
+    };
+  },
+  methods: {
+    login() {
+      axios.post(`${this.baseUrl}/admin/signin`, this.userData)
+      .then(res => {
+        const { token, expired } = res.data;
+        document.cookie = `wrightyuToken=${token};expires=${new Date(expired)}; path=/`;
+        alert("登入成功");
+        window.location.assign("./products.html");
+      })
+      .catch(err => {
+        alert("登入失敗");
+        console.log(err.data.message);
+      });
+    }
+  },
+};
+
+Vue.createApp(app).mount("#app");
+
---
+++ products.js
@@ -0,0 +1,38 @@
+const app = {
+  data() {
+    return {
+      baseUrl: "https://vue3-course-api.hexschool.io/v2",
+      userPath: "wrightyu",
+      tempProduct: {},
+      products: [],
+    };
+  },
+  methods: {
+    checkAdmin() {
+      axios.post(`${this.baseUrl}/api/user/check`)
+      .then(() => {
+        this.getProducts();
+      })
+      .catch(err => {
+        alert(err.data.message);
+        window.location.assign("./index.html");
+      })
+    },
+    getProducts() {
+      axios.get(`${this.baseUrl}/api/${this.userPath}/admin/products`)
+      .then(res => {
+        this.products = res.data.products;
+      })
+      .catch(err => {
+        alert(err.data.message);
+      })
+    },
+  },
+  mounted() {
+    const token = document.cookie.replace(/(?:(?:^|.*;\s*)wrightyuToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
+    axios.defaults.headers.common.Authorization = token;
+    this.checkAdmin();
+  },
+};
+
+Vue.createApp(app).mount("#app");
